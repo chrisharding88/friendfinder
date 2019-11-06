3bd75ca5799e2139a9d3c95aad1c09a918a2687f
Christopher Harding <chrisharding88@gmail.com>
2019-11-06 00:40:28 -0500

    added content in app/public/home and app/public/survey.html

--- app/server.js
+++ app/server.js
@@ -1,16 +1,23 @@
+
+// Setting up Dependencies
 var express = require ("express");
 var path = require ("path");
+var nodemon = require("nodemon")
 
+// Setting up express
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
+// Express data capabilities
+app.use(express.static('app/public'))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Router
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+// PORT Listener
 app.listen(PORT, function(){
     console.log(`App listening on PORT: ${PORT}`);
 })
