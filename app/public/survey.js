bf7512ad25d8115b8fff4c97b27490ca15fb8788
Christopher Harding <chrisharding88@gmail.com>
2019-11-09 08:42:47 -0500

    Added the survey.js w/ value plus added that model in the survey.html

---
+++ app/public/survey.js
@@ -0,0 +1,45 @@
+var configuration ={
+
+}
+
+
+$('#submit').click(function(){
+    event.preventDefault()
+    var userData = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+            $("#q1").val(),            
+            $("#q2").val(),
+            $("#q3").val(),
+            $("#q4").val(),
+            $("#q5").val(),
+            $("#q6").val(),
+            $("#q7").val(),
+            $("#q8").val(),
+            $("#q9").val(),
+            $("#q10").val()
+        ]
+
+    }
+
+
+    console.log(userData);
+$.post("/api/friends", userData,  function(data){
+    console.log(data);
+
+    $("#matchFullName").text(data.name);
+    $("#matchImage").attr("src", data.photo);
+
+})
+
+
+
+
+
+
+
+
+
+
+});
